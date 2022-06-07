ccf8e92520e8fd364b846539c61d5ff361ebda8d
Mark Warneke <warneke.mark@gmail.com>
2022-06-07 08:16:26 +0000

    add sample

--- index.php
+++ index.php
@@ -1,27 +1,36 @@
 <?php
 
-/*----------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See LICENSE in the project root for license information.
- *---------------------------------------------------------------------------------------*/
+$api_token = $_ENV["ACCESS_TOKEN"];
 
-function sayHello($name) {
-	echo "Hello $name!";
-}
+$url="https://api.mmmint.ai/fahrzeugschein/v1/fahrzeugschein?access_token=".$api_token;
+
+$headers = array(
+	"Content-Type:multipart/form-data",
+	"Accept:application/json"
+);
+
+$fileName = "registration-anonymous.png";
+$filePath = $_SERVER['DOCUMENT_ROOT'] . $fileName;
+
+$fields = [
+	'file' => new \CurlFile($filePath, 'image/png', $fileName)
+];
+
+$curl = curl_init($url);
+
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_POSTFIELDS, $fields);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+
+//debug
+
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+$resp1 = curl_exec($curl);
+
+print_r($resp1);
 
 ?>
-
-<html>
-	<head>
-		<title>Visual Studio Code Remote :: PHP</title>
-	</head>
-	<body>
-		<?php 
-		
-		sayHello('remote world');
-			
-		phpinfo(); 
-			
-		?>
-	</body>
-</html>
